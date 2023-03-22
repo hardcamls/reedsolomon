032641bb54d621102d79068ac30a7d0a90fa40ed
andrewray <evilkidder@gmail.com>
2023-03-22 20:54:01 +0000

    new codec test ... in progress

--- bin/hw_file.mli
+++ bin/hw_file.mli
@@ -1,1 +1,16 @@
 val command : Core.Command.t
+
+module New : sig
+  module Encode : sig
+    type t
+
+    val create
+      :  params:Reedsolomon.Iter_codec.params
+      -> get_byte:(unit -> Char.t)
+      -> put_byte:(Char.t -> unit)
+      -> total_length_in_bits:int
+      -> t
+
+    val encode : t -> unit
+  end
+end
